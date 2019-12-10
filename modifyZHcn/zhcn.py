8c9c6ea9181b1716cfe555000fcc179e42f7be8e
ackurdeeve <chenxuxuan@wust.edu.cn>
2019-12-11 00:24:22 +0800

    encoding done

--- modifyZHcn/zhcn.py
+++ modifyZHcn/zhcn.py
@@ -10,14 +10,19 @@ class Zhcn:
         # print(flow.request.url)
         # print(flow.request.host)
         if flow.request.host == "www.ackurdeeve.com":
-            html = requests.get("https://www.ackurdeeve.com")
+            url = flow.request.url
+            print('-' * 30)
+            print(url)
+            html = requests.get(url)
             print('encoding:', html.encoding)
             print('apparent_encoding:', html.apparent_encoding)
-            html.encoding = ('utf8')
-            text = html.text
-            text = text.replace("扶墙机场推荐-GLaDOS 通用网络游戏加速器", "Hello World")
-            # print(html.text)
-            flow.response.set_text(text)
+
+            if html.encoding != 'None':
+                html.encoding = ('utf8')
+                text = html.text
+                text = text.replace("畅游星海", "Hello World")
+                # print(html.text)
+                flow.response.set_text(text)
             print('-' * 30)
 
         if flow.request.host == "www.so.com":
