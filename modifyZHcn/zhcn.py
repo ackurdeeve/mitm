f93359366fa9af9f87043166fe78a83982202c78
ackurdeeve <chenxuxuan@wust.edu.cn>
2019-12-13 00:09:31 +0800

    update termius vip

--- modifyZHcn/zhcn.py
+++ modifyZHcn/zhcn.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import requests
+import json
 
 
 class Zhcn:
@@ -16,7 +17,6 @@ class Zhcn:
             html = requests.get(url)
             print('encoding:', html.encoding)
             print('apparent_encoding:', html.apparent_encoding)
-
             if html.encoding != 'None':
                 html.encoding = ('utf8')
                 text = html.text
@@ -29,3 +29,20 @@ class Zhcn:
             html = flow.response.text
             print(html)
             print('-' * 30)
+
+        if flow.request.host == "education.github.com":
+            text5 = flow.response.text
+            parsed_json5 = json.loads(text5)
+            parsed_json5["student"] = True
+            text5 = json.dumps(parsed_json5)
+            flow.response.set_text(text5)
+            print('-' * 30)
+
+        if flow.request.url.startswith("https://api.termius.com/api/v3/bulk/account/"):
+            obj = json.loads(flow.response.get_text())
+            obj["account"]['pro_mode'] = True
+            obj['account']['plan_type'] = "Premium"
+            obj['account']['user_type'] = "Premium"
+            obj['account']['current_period']['until'] = "2099-10-10T03:27:34"
+            flow.response.set_text(json.dumps(obj))
+
