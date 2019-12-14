ba9cebe4bae1dbdd336754481e451af2ab30e365
ackurdeeve <chenxuxuan@wust.edu.cn>
2019-12-14 23:16:56 +0800

    change upstream -> socks5 使用clash分流规则 避免http全局代理导致的性能问题

--- Sys_autostart/mitmscripts.bat
+++ Sys_autostart/mitmscripts.bat
@@ -1,2 +1,2 @@
 taskkill /F /IM  mitmweb.exe /T
-D:\Code\python\mitm\dev\Scripts\mitmweb --no-web-open-browser -p 2080 --anticache -m upstream:http://127.0.0.1:1081 -s D:\Code\python\mitm\Sys_autostart\script.py
+D:\Code\python\mitm\dev\Scripts\mitmweb --no-web-open-browser -p 2080 --anticache -m socks5 -s D:\Code\python\mitm\Sys_autostart\script.py
